560994012edd6223545f5f6d2618f65a6671bd4b
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-02-17 12:15:14 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,125 @@
+import './css/styles.css';
+import ImgApiService from './js/img-service';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
+const refs = {
+    form: document.querySelector('#search-form'),
+    input: document.querySelector('input'),
+    searchBtn: document.querySelector('button'),
+    gallery: document.querySelector('.gallery'),
+    loadBtn: document.querySelector('.load-more'),
+};
+
+
+const imgApiService = new ImgApiService();
+let searchQuery = '';
+
+// --------------------------------------------
+
+// --------------------------------------------
+
+refs.form.addEventListener('submit', onSearchImg);
+refs.loadBtn.addEventListener('click', onLoadMoreImg);
+
+
+function onSearchImg(e) {
+    e.preventDefault();
+    
+
+    imgApiService.query = e.currentTarget.elements.searchQuery.value;
+
+    if (imgApiService.query === '') {
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        return;
+    }
+
+    imgApiService.resetPage();
+
+    imgApiService.fetchImg()
+        .then(hits => {
+            clearArticlesContainer();
+            appendArticlesMarkup(hits);
+            simplelightbox.refresh();
+            refs.loadBtn.classList.remove('is-hidden');
+
+            scrollPage();
+            // gallarySlider.refresh();
+        });
+};
+
+
+function onLoadMoreImg() {
+    imgApiService.fetchImg()
+        .then(hits => {
+            appendArticlesMarkup(hits)
+            scrollPage();
+            simplelightbox.refresh();            
+        });
+    
+    // gallarySlider.refresh();
+}
+
+
+function appendArticlesMarkup(hits) {
+    refs.gallery.insertAdjacentHTML('beforeend', createGallery(hits));
+}
+
+
+function createGallery({hits}) {
+    const galleryMarkup = hits
+        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+            return `<a class="link-card" href=${largeImageURL}>
+                        <div class="photo-card">
+                            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+                            <div class="info">
+                                <p class="info-item">
+                                    <b class="info-item-value">Likes</b>
+                                    ${likes}
+                                </p>
+                                <p class="info-item">
+                                    <b class="info-item-value">Views</b>
+                                    ${views}
+                                </p>
+                                <p class="info-item">
+                                    <b class="info-item-value">Comments</b>
+                                    ${comments}
+                                </p>
+                                <p class="info-item">
+                                    <b class="info-item-value">Downloads</b>
+                                    ${downloads}
+                                </p>
+                            </div>
+                        </div>
+                    </a>`
+        })
+        .join('');
+    
+    return galleryMarkup;
+}
+
+
+function clearArticlesContainer() {
+    refs.gallery.innerHTML = '';
+}
+
+const simplelightbox = new SimpleLightbox('gallery a', {
+    overlayOpacity: 0.9,
+    captionsData: "alt",
+    captionDelay: 250,
+    animationSpeed: 500,
+});
+
+
+function scrollPage() {
+    const { height: cardHeight } = document
+        .querySelector(".gallery")
+        .firstElementChild.getBoundingClientRect();
+    
+    window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+    });
+};
---
+++ src/js/img-service.js
@@ -0,0 +1,80 @@
+// ---------------------------IMPORT------------------------------------
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
+
+
+
+
+// ---------------------------EXPORT------------------------------------
+axios.defaults.baseURL = 'https://pixabay.com/api';
+const API_KEY = '33500508-b4271a177ba3ac813eaf35292&q';
+const PARAM = 'image_type=photo&orientation=horizontal&safesearch=true';
+
+
+// export default class ImgApiService {
+class ImgApiService {    
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+
+    async fetchImg() {
+        
+        const URL = `/?key=${API_KEY}&q=${this.searchQuery}&${PARAM}&per_page=40&page=${this.page}`
+        const response = await axios.get(`/?key=${API_KEY}&q=${this.searchQuery}&${PARAM}&per_page=40&page=${this.page}`);
+
+        return fetch(URL)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(response.status);
+                }
+
+                return response.json();
+            })
+            .then(hits => {
+                // console.log(hits.hits.length);
+                // console.log(hits.totalHits);
+                this.incrementPage();
+
+                if (hits.totalHits > 0) {
+                    Notify.info(`Hooray! We found ${hits.totalHits} images.`);
+                }
+                
+
+                // if(hits.totalHits === )
+                // const totalNumberImg = [];
+                // const currentNumberImg = hits.hits.length;
+                    
+                // totalNumberImg.push(currentNumberImg);
+
+                // console.log(totalNumberImg);
+
+                return hits;
+            })        
+    }
+    
+    resetPage() {
+        this.page = 1;
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+
+    setTotal(total) {
+        this.#totalPages = total;
+    }
+};
+
+
+
+
